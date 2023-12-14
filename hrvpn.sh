8ebe180cae945f877218af7fad2b5b037c2f3531
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-14 06:46:31 -0800

    Create hrvpn.sh

---
+++ hrvpn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install dependencies
+apt install -y wget screen
+apt update -y && apt upgrade -y
+apt install lolcat -y
+gem install lolcat
+
+# Download and run setup.sh
+wget -q https://raw.githubusercontent.com/botak8/san/main/hr.sh
+chmod +x hr.sh
+screen -S install ./hr.sh
